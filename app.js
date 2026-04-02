266400b2da561b32c0206a28e5ff212f556e36dd
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-04-02 18:44:33 +0200

    conversione REST

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const postsRouter = require('./routers/posts')
 
 app.get('/' , (req, res) =>{
     res.send("pagina principale")
 })
 
+app.use('/posts' , postsRouter)
 app.listen(port, () => {
     console.log(`Server in ascolto su http://localhost:${port}`);
 });
+
--- routers/posts.js
+++ routers/posts.js
@@ -1,7 +1,67 @@
 const express = require('express')
 const router = express.Router()
 
+const posts = [
+    {
+        id: 1,
+        titolo: "Ciambellone",
+        contenuto: "La ricetta della nonna per un ciambellone soffice.",
+        immagine: "images/ciambellone.jpeg",
+        tags: ["Dolci", "Cucina"]
+    },
+    {
+        id: 2,
+        titolo: "Cracker alla Barbabietola",
+        contenuto: "Uno snack salutare e colorato.",
+        immagine: "images/cracker_barbabietola.jpeg",
+        tags: ["Snack", "Salute"]
+    },
+    {
+        id: 3,
+        titolo: "Pane fritto dolce",
+        contenuto: "Il gusto della semplicità fritto in padella.",
+        immagine: "images/pane_fritto.jpeg",
+        tags: ["Dolci", "Tradizione"]
+    },
+    {
+        id: 4,
+        titolo: "Pasta allo Scoglio",
+        contenuto: "Tutto il sapore del mare in un primo piatto.",
+        immagine: "images/pasta_scoglio.jpeg",
+        tags: ["Primi", "Pesce"]
+    },
+    {
+        id: 5,
+        titolo: "Torta Paesana",
+        contenuto: "La torta di pane tipica della Brianza.",
+        immagine: "images/torta_paesana.jpeg",
+        tags: ["Dolci", "Recupero"]
+    }
+];
+
+
 router.get('/' ,(req, res) => {
-    res.send("lista post")
+    res.send(posts)
 })
 
+router.get('/:id', (req, res) => {
+    res.send('dettagli post')
+})
+
+router.post('/' , (req, res) => {
+    res.send('creazione post')
+})
+
+router.put('/:id' , (req, res) => {
+    res.send('modifica del post')
+})
+
+router.patch('/:id' , (req, res) => {
+    res.send('modifica del post')
+})
+
+router.delete('/:id' , (req, res) => {
+    res.send('post eliminato')
+})
+
+module.exports = router
